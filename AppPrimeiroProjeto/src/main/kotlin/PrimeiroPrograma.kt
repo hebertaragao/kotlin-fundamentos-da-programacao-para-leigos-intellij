118c94f6736f5ffef86425d8d27cc70e87a071d6
Hebert Aragão da Silva <57552137+hebertaragao@users.noreply.github.com>
2023-04-27 18:45:39 -0300

    Tipos de variáveis mutáveis e não mutáveis

--- AppPrimeiroProjeto/src/main/kotlin/PrimeiroPrograma.kt
+++ AppPrimeiroProjeto/src/main/kotlin/PrimeiroPrograma.kt
@@ -1,4 +1,13 @@
 fun main(){
     // Meu primeiro programa
     print("Primeiro programa em kotlin...")
+
+    println("Qual o seu nome? ")
+    var nome: String = readLine()!!.toString()
+
+    println("Olá! $nome. Muito prazer!")
+
+    println("Qual sua idade? ")
+    var idade: Int = readLine()!!.toInt()
+    println("Sua idade é: $idade.")
 }
---
+++ AppQuartoProjeto/src/main/kotlin/ClcularRaioCirculo.kt
@@ -0,0 +1,11 @@
+import kotlin.math.PI
+
+fun main(){
+    var circulo = 15 // variável mutável
+    //val  pi = 3.141516 // variável não mutável
+
+
+    var valorDoRaio = circulo / (2* PI)
+    print("Círculo mede: $circulo\n")
+    print("Valor do raio do Círculo é: $valorDoRaio\n")
+}
